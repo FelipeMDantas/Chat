d80adfa4116786e30848a8ed0f7c8fe158d8d118
FelipeMDantas <felipemdantas@terra.com.br>
2022-09-17 23:34:36 -0300

    Login page developed and components defined.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span className="logo">Chat</span>
+      <div className="user">
+        <img
+          src="https://images.pexels.com/photos/11500404/pexels-photo-11500404.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+        />
+        <span>John</span>
+        <button>logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Sidebar;
