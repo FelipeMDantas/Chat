aa1631d6546df2f05d6461daf7481b494d934180
FelipeMDantas <felipemdantas@terra.com.br>
2022-09-19 23:58:04 -0300

    Input component and message screen elaborated.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,22 @@
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/11500404/pexels-photo-11500404.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://images.pexels.com/photos/11500404/pexels-photo-11500404.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
