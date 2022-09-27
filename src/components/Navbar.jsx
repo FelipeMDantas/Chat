6ab3f154bdfc3bc7953e83bbcbc9c2d06dbb771f
FelipeMDantas <felipemdantas@terra.com.br>
2022-09-26 23:26:18 -0300

    Firebase procedures concerning chat sessions data manipulation added.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import { signOut } from "firebase/auth";
+import { useContext } from "react";
 import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <span className="logo">Chat</span>
       <div className="user">
-        <img
-          src="https://images.pexels.com/photos/11500404/pexels-photo-11500404.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-          alt=""
-        />
-        <span>John</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,88 @@
+import { useState, useContext } from "react";
+import {
+  collection,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  const handleSelect = async () => {
+    //Check whether group (chats in firestore) exists; if not, then, create it.
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+
+    try {
+      const res = await getDocs(db, "chats", combinedId);
+
+      if (!res.exists()) {
+        //Create a chat in the chats collection
+        await setDoc(doc, (db, "chats", combinedId), { messages: [] });
+
+        //Create user chats
+        await updateDoc(doc, (db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Find a user"/>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/11500404/pexels-photo-11500404.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-          alt=""
+        <input
+          type="text"
+          placeholder="Find a user"
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={handleKey}
         />
-        <div className="userChatInfo">
-          <span>Jane</span>
-        </div>
       </div>
+      {err && <span>User not found</span>}
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
